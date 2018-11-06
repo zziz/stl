69d094fd925e447a7abf03a2b3f3b1245d5101ac
zziz <fvzaur@gmail.com>
2018-11-06 21:20:47 +0900

    is_until algorithms

--- README.md
+++ README.md
@@ -304,3 +304,31 @@ Is heap?                  0
 Is heap?                  1
 Predicate: is positive?   1
 ```
+
+### is_####_until Algorithms
+```cpp
+std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+std::cout << "Current State     ";
+for(auto& i : numbers)    std::cout << i << " ";    
+std::cout << std::endl;  
+
+// is_sorted_until
+// More @ https://en.cppreference.com/w/cpp/algorithm/is_sorted_until
+auto sorted = std::is_sorted_until(numbers.begin(), numbers.end());
+if(sorted != numbers.end()) std::cout << "Is sorted until " << *sorted << std::endl;
+else                        std::cout << "Is sorted until the end" << std::endl;
+
+// is_heap_until
+// More @ https://en.cppreference.com/w/cpp/algorithm/is_heap_until
+auto heap = std::is_heap_until(numbers.begin(), numbers.end());
+if(heap != numbers.end())   std::cout << "Is heap until " << *heap << std::endl;
+else                        std::cout << "Is heap until the end" << std::endl;
+```
+
+#### Output 
+```
+Current State     1 2 3 4 5 6 7 8 9 10 
+Is sorted until the end
+Is heap until 2
+```
--- stl.cpp
+++ stl.cpp
@@ -246,9 +246,23 @@ void is_foo_algorithms(){
 }
 
 void is_foo_until_algorithms(){
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    std::cout << "Current State     ";
+    for(auto& i : numbers)    std::cout << i << " ";    
+    std::cout << std::endl;  
+
     // is_sorted_until
+    // More @ https://en.cppreference.com/w/cpp/algorithm/is_sorted_until
+    auto sorted = std::is_sorted_until(numbers.begin(), numbers.end());
+    if(sorted != numbers.end()) std::cout << "Is sorted until " << *sorted << std::endl;
+    else                        std::cout << "Is sorted until the end" << std::endl;
+
     // is_heap_until
-    // is_partitioned_until
+    // More @ https://en.cppreference.com/w/cpp/algorithm/is_heap_until
+    auto heap = std::is_heap_until(numbers.begin(), numbers.end());
+    if(heap != numbers.end())   std::cout << "Is heap until " << *heap << std::endl;
+    else                        std::cout << "Is heap until the end" << std::endl;
 }
 
 void querying_algorithms(){
@@ -492,8 +506,8 @@ int main(){
     // partition_algorithms();
     // permutation_algorithms();
     // stable_foo_algorithms();
-    is_foo_algorithms();
-    // is_foo_until_algorithms();
+    // is_foo_algorithms();
+    is_foo_until_algorithms();
     // querying_algorithms();
     // set_algorithms();
     // movers_algorithms();
